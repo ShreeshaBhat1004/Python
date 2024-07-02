e511103a9dba55de6b6c1a14a244d86bd25cd407
Shreesha R Bhat <srbhat1004@gmail.com>
2024-07-02 10:57:17 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -22,3 +22,18 @@ def merge_sort(arr):
             arr[k] = right_half[j]
             j += 1
             k += 1
+####################################################
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1
