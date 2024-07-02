b41c11947436ad27a3327462f0f8e66587b85d1b
Shreesha R Bhat <srbhat1004@gmail.com>
2024-07-02 10:55:26 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,9 @@
+agencies = {"CBI": "Central Bureau of investigation""FBI": "Foreign 
+direct investment", "NIA": "National investion agency",”SSB":"Service 
+selection board","WBA" : "Works Progress administration" }
+print (agencies)
+print ("******")
+type (agencies)
+Add the map of acronym BSE "Bombay stock exchange":
+agencies ["BSE"] = "Bombay stock exchange"
+print (agencies)
